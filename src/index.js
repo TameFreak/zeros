afc8a5ef3710a4a7abf6344237e4219e701ea885
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-09-11 19:16:12 +0300

    -_-

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let counter = 0;
+
+  for (let i = 5; i <= number; i += 5){
+    let elem = i;
+    while(elem % 5 === 0){
+      counter++;
+      elem = elem / 5;
+    }
+  }
+
+  return counter;
 }
